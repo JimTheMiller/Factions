b8b95d3a220d8b01ae52363540e2a4f3ca88d648
bleevo <bleevo@gmail.com>
2011-04-07 01:50:52 +1000

    removed ability to steal claims. use unclaim instead.

--- src/com/bukkit/mcteam/factions/commands/FCommandClaim.java
+++ src/com/bukkit/mcteam/factions/commands/FCommandClaim.java
@@ -4,7 +4,6 @@ import com.bukkit.mcteam.factions.Board;
 import com.bukkit.mcteam.factions.Conf;
 import com.bukkit.mcteam.factions.FLocation;
 import com.bukkit.mcteam.factions.Faction;
-import com.bukkit.mcteam.factions.struct.Relation;
 import com.bukkit.mcteam.factions.struct.Role;
 
 public class FCommandClaim extends FBaseCommand {
@@ -16,7 +15,8 @@ public class FCommandClaim extends FBaseCommand {
 	}
 	
 	public void perform() {
-		if ( ! assertHasFaction()) {
+		
+		if (!assertHasFaction()) {
 			return;
 		}
 		
@@ -24,69 +24,31 @@ public class FCommandClaim extends FBaseCommand {
 		FLocation flocation = new FLocation(me);
 		Faction otherFaction = Board.getFactionAt(flocation);
 		
+		if (!assertMinRole(Role.MODERATOR)) {
+			return;
+		}
+
 		if (myFaction == otherFaction) {
 			sendMessage("You already own this land.");
 			return;
 		}
-		
-		if (!assertMinRole(Role.MODERATOR)) {
+
+		if (!otherFaction.isNone()) {
+			sendMessage("This land is already claimed. Try /f unclaim");
 			return;
 		}
-		
 		
 		if (myFaction.getLandRounded() >= myFaction.getPowerRounded()) {
 			sendMessage("You can't claim more land! You need more power!");
 			return;
 		}
-		
-		if (otherFaction.getRelation(me) == Relation.ALLY) {
-			sendMessage("You can't claim the land of your allies.");
-			return;
-		}
-		
+	
 		if (otherFaction.isSafeZone()) {
 			sendMessage("You can not claim a SafeZone.");
 			return;
 		}
 		
-		/*
-		long worldX = flocation.getX() * (long)flocation.getCellSize();
-		long worldZ = flocation.getZ() * (long)flocation.getCellSize();
-		
-		Server server = Factions.instance.getServer();
-		Player player = server.getPlayer(me.getName());
-		World world = player.getWorld();
-		
-		for (int i = 0; i < 1; i++) {
-			world.getBlockAt((int)(worldX), 127 - i, (int)(worldZ)).setType(Material.GRAVEL);
-			world.getBlockAt((int)(worldX), 127 - i, (int)(worldZ + (long)flocation.getCellSize())).setType(Material.GRAVEL);
-			world.getBlockAt((int)(worldX + (long)flocation.getCellSize()), 127 - i, (int)worldZ).setType(Material.GRAVEL);
-			world.getBlockAt((int)(worldX + (long)flocation.getCellSize()), 127 - i, (int)(worldZ + (long)flocation.getCellSize())).setType(Material.GRAVEL);
-		}
-		*/
-		
-		if (otherFaction.isNone()) {
-			myFaction.sendMessage(me.getNameAndRelevant(myFaction)+Conf.colorSystem+" claimed some new land :D");
-		} else { //if (otherFaction.isNormal()) {
-			
-			if ( ! otherFaction.hasLandInflation()) {
-				 // TODO more messages WARN current faction most importantly
-				sendMessage(me.getRelationColor(otherFaction)+otherFaction.getTag()+Conf.colorSystem+" owns this land and is strong enough to keep it.");
-				return;
-			}
-			
-			int plotsAround = Board.numberSurroundingPlots(flocation);
-			if (plotsAround > 2) {
-				sendMessage("There are still " + plotsAround + " surrounding plots.");
-				return;
-			}
-			
-			// ASDF claimed some of your land 450 blocks NNW of you.
-			// ASDf claimed some land from FACTION NAME
-			otherFaction.sendMessage(me.getNameAndRelevant(otherFaction)+Conf.colorSystem+" stole some of your land :O");
-			myFaction.sendMessage(me.getNameAndRelevant(myFaction)+Conf.colorSystem+" claimed some land from "+otherFaction.getTag(myFaction));
-		}
-		
+		myFaction.sendMessage(me.getNameAndRelevant(myFaction) + Conf.colorSystem + " claimed some new land :D");
 		Board.setFactionAt(myFaction, flocation);
 	}
 	
