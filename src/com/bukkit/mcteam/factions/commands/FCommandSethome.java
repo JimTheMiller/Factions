6fbcb258e489172fc105d14d696d792cc231437c
bleevo <bleevo@gmail.com>
2011-04-07 01:52:28 +1000

    Can only set home within a claim that your faction owns.

--- src/com/bukkit/mcteam/factions/commands/FCommandSethome.java
+++ src/com/bukkit/mcteam/factions/commands/FCommandSethome.java
@@ -1,40 +1,44 @@
 package com.bukkit.mcteam.factions.commands;
 
-//import com.bukkit.mcteam.factions.Conf;
-//import com.bukkit.mcteam.factions.Faction;
-//import com.bukkit.mcteam.factions.struct.Role;
+import com.bukkit.mcteam.factions.Board;
+import com.bukkit.mcteam.factions.Conf;
+import com.bukkit.mcteam.factions.FLocation;
+import com.bukkit.mcteam.factions.Faction;
+import com.bukkit.mcteam.factions.struct.Role;
 
 public class FCommandSethome extends FBaseCommand {
 	
 	public FCommandSethome() {
 		aliases.add("sethome");
-		
 		helpDescription = "Set the faction home";
 	}
 	
 	public void perform() {
-		/*
-		if ( ! assertHasFaction()) {
+		
+		if (!assertHasFaction()) {
 			return;
 		}
 		
-		if ( ! assertMinRole(Role.MODERATOR)) {
+		if (!assertMinRole(Role.MODERATOR)) {
 			return;
 		}
 		
-		if ( ! Conf.homesEnabled) {
+		if (!Conf.homesEnabled) {
 			me.sendMessage("Sorry, Faction homes are disabled on this server.");
 			return;
 		}
-		
-		// TODO may only be inside faction territory
-		
+				
 		Faction myFaction = me.getFaction();
-		myFaction.setHome(player.getLocation());
+		FLocation flocation = new FLocation(me);
+		Faction otherFaction = Board.getFactionAt(flocation);
 		
-		myFaction.sendMessage(me.getNameAndRelevant(myFaction)+Conf.colorSystem+" set the home for your faction. You can now use:");
-		myFaction.sendMessage(new FCommandHome().getUseageTemplate(true, true));
-		*/
+		if (myFaction == otherFaction) {
+			myFaction.setHome(player.getLocation());
+			myFaction.sendMessage(me.getNameAndRelevant(myFaction)+Conf.colorSystem+" set the home for your faction. You can now use:");
+			myFaction.sendMessage(new FCommandHome().getUseageTemplate(true, true));
+		} else {
+			me.sendMessage("You can only set home within your factions claims");
+		}
 	}
 	
 }
